65541c436a7e63ab09e5a803b5ef3f5967595eb2
eegorov <eegorov@kazanworkout.ru>
2022-11-05 17:08:09 +0300

    SD-10. косметические правки

--- app/Shared/Infrastructure/Controllers/HealthCheckActionController.php
+++ app/Shared/Infrastructure/Controllers/HealthCheckActionController.php
@@ -7,9 +7,9 @@ namespace App\Shared\Infrastructure\Controllers;
 use Illuminate\Http\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/health-check', name: 'health_check', methods: ['GET'])]
 final class HealthCheckActionController extends Controller
 {
+    #[Route('/health-check', name: 'health_check', methods: ['GET'])]
     public function __invoke(): JsonResponse
     {
         return new JsonResponse(['status' => 'ok']);
